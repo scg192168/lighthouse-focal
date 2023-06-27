d36e0701261c6a01bc5885318e7f6588959daec4
Shyamal <scghoshbc@gmail.com>
2023-06-26 23:53:30 -0700

    corrected version

--- reverse.js
+++ reverse.js
@@ -1,8 +1,8 @@
-var input = process.argv[2];
+let input = process.argv[2];
 
-if (input)
+if (input) {
   console.log(reverse(input));
-
+}
 function reverse(original) {
   return orignal.split('').reverseList().join('');
 }
